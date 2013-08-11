479c5714771563ccba0adde320b9fababc2fc6ee
柏健 <jian.baij@gmail.com>
2013-08-11 23:17:41 +0800

    add FindBin example

---
+++ perl/Findbin.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+print $FindBin::Bin;
+print "\n";
+print $FindBin::RealBin;
+print "\n";
+print $FindBin::Script;
+print "\n";
+print $FindBin::RealScript;
