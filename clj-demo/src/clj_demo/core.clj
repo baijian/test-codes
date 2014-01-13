0eb7889bd15d5bb5469ee29af69b4ebe20e3b6cc
柏健 <jian.baij@gmail.com>
2014-01-13 23:21:32 +0800

    for and doseq demo

--- clj-demo/src/clj_demo/core.clj
+++ clj-demo/src/clj_demo/core.clj
@@ -4,5 +4,11 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (for [x [:a :b], y (range 5) :when (odd? y)] [prn x y])
-  (doseq [x [:a :b], y (range 5) :when (odd? y)] (prn x y)))
+  (println "for example:")
+  (dorun (for [x [:a :b]
+               y (range 5) :when (odd? y)]
+           (prn x y)))
+  (println "doseq example:")
+  (doseq [x [:a :b]
+          y (range 5) :when (odd? y)]
+    (prn x y)))
